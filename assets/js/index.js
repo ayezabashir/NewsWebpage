3756da685a8947779063812802a58a8a05501159
Ayeza <ayezabashir442@gmail.com>
2023-02-15 21:04:43 +0500

    theme switch

---
+++ assets/js/index.js
@@ -0,0 +1,37 @@
+// grab elements
+
+const selectElement = selector => {
+    const element = document.querySelector(selector);
+    if (element) return element;
+    else throw new Error(`Element does not exist, make sure ${selector} exists`);
+}
+
+// Nav styles on scroll
+const scrollHeader = () => {
+    const headerElement = document.querySelector('#header');
+    if (this.scrollY >= 15) {
+        headerElement.classList.add('activated');
+    }
+    else {
+        headerElement.classList.remove('activated');
+    }
+}
+window.addEventListener('scroll', scrollHeader);
+
+// Open Menu
+const menuToggleIcon = selectElement('#menu-toggle-icon');
+const toggleMenu = () => {
+    const mobileMenu = selectElement('#menu');
+    mobileMenu.classList.toggle('activated');
+    menuToggleIcon.classList.toggle('activated');
+}
+
+menuToggleIcon.addEventListener('click', toggleMenu);
+
+// Switch themes/Add to local Storage
+const bodyElement = document.body;
+const themeToggleBtn = selectElement('#theme-toggle-btn');
+
+themeToggleBtn.addEventListener('click', () => {
+    bodyElement.classList.toggle('light-theme');
+})
